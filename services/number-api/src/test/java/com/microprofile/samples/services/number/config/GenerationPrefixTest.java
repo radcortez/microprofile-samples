907e27e3350ece8138ebe842b6b813052bc12304
Roberto Cortez <radcortez@yahoo.com>
2021-06-01 18:21:09 +0100

    Try to reproduce quarkus-17525.

--- services/number-api/src/test/java/com/microprofile/samples/services/number/config/GenerationPrefixTest.java
+++ services/number-api/src/test/java/com/microprofile/samples/services/number/config/GenerationPrefixTest.java
@@ -1,30 +1,46 @@
 package com.microprofile.samples.services.number.config;
 
-import org.eclipse.microprofile.config.ConfigProvider;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigValue;
 import org.junit.jupiter.api.Test;
 
-import static com.microprofile.samples.services.number.config.GenerationPrefix.BK;
-import static com.microprofile.samples.services.number.config.GenerationPrefix.MV;
-import static com.microprofile.samples.services.number.config.GenerationPrefix.UN;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
+import static com.microprofile.samples.services.number.config.GenerationPrefix.BK;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+@TestProfile(GenerationPrefixTest.TestProfile.class)
 class GenerationPrefixTest {
+    @Inject
+    Config config;
+    
     @Test
     void prefix() {
-        assertEquals(BK, ConfigProvider.getConfig().getValue("generation.prefix", GenerationPrefix.class));
+        assertEquals(BK, config.getValue("generation.prefix", GenerationPrefix.class));
+        ConfigValue configValue = config.getConfigValue("generation.prefix");
+
+        assertTrue(configValue.getSourceName().contains("config.properties"));
+        assertEquals("config.properties", config.getConfigValue("smallrye.config.locations").getValue());
     }
 
-    @Test
-    void override() {
-        System.setProperty("generation.prefix", "MV");
-        assertEquals(MV, ConfigProvider.getConfig().getValue("generation.prefix", GenerationPrefix.class));
-        System.clearProperty("generation.prefix");
-    }
+    public static class TestProfile implements QuarkusTestProfile {
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            Map<String, String> configs = new HashMap<>();
+            configs.put("smallrye.config.locations", "config.properties");
+            return configs;
+        }
 
-    @Test
-    void undefined() {
-        System.setProperty("generation.prefix", "X");
-        assertEquals(UN, ConfigProvider.getConfig().getValue("generation.prefix", GenerationPrefix.class));
-        System.clearProperty("generation.prefix");
+        @Override
+        public String getConfigProfile() {
+            return "test";
+        }
     }
 }
